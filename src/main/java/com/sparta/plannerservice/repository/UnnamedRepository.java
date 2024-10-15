f16b7110484beabb28996e5602d0602b4af41d58
34-43 <jehyuck3443@gmail.com>
2024-10-15 17:37:52 +0900

    chore: removed domain example classes

--- src/main/java/com/sparta/plannerservice/repository/UnnamedRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.plannerservice.repository;
-
-import com.sparta.plannerservice.entity.Unnamed;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UnnamedRepository extends JpaRepository<Unnamed, Long> {
-
-}
