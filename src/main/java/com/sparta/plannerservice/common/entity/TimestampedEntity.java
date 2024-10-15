abf772a2b6a1f26d28d476c1826cd227092957de
34-43 <jehyuck3443@gmail.com>
2024-10-15 17:35:29 +0900

    feat: added common Entity parents

---
+++ src/main/java/com/sparta/plannerservice/common/entity/TimestampedEntity.java
@@ -0,0 +1,24 @@
+package com.sparta.plannerservice.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimestampedEntity extends LongIdKeyEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+}
