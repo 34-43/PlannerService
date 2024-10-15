991b52e4a00fde0650e5e92a8116e07690d486f0
34-43 <jehyuck3443@gmail.com>
2024-10-15 17:36:09 +0900

    feat: added User Dto (merge req, read res)

---
+++ src/main/java/com/sparta/plannerservice/user/dto/MergeUserReqDto.java
@@ -0,0 +1,28 @@
+package com.sparta.plannerservice.user.dto;
+
+import com.sparta.plannerservice.common.dto.MergeReqDto;
+import com.sparta.plannerservice.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MergeUserReqDto implements MergeReqDto<User> {
+
+    @NotBlank
+    @Size(min = 1, max = 12)
+    private String username;
+
+    @NotBlank
+    @Size(min = 1, max = 100)
+    @Email
+    private String email;
+
+    @Override
+    public User toEntity() {
+        return new User(username, email);
+    }
+}
---
+++ src/main/java/com/sparta/plannerservice/user/dto/ReadUserResDto.java
@@ -0,0 +1,20 @@
+package com.sparta.plannerservice.user.dto;
+
+import com.sparta.plannerservice.common.dto.ReadTimestampedEntityReqDto;
+import com.sparta.plannerservice.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReadUserResDto extends ReadTimestampedEntityReqDto {
+    private String username;
+    private String email;
+
+    public ReadUserResDto(User user) {
+        // 부모 Dto 클래스에서 정의된 필드(id 등)의 대입은 super 로 수행
+        super(user);
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+    }
+}
