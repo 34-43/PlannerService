7ff11f2aacccd90bf405bcb5684a3fbfb5117465
34-43 <jehyuck3443@gmail.com>
2024-10-16 17:03:18 +0900

    feat: assigned @ManyToMany relation (User <-> Plan)

---
+++ src/main/java/com/sparta/plannerservice/plan/entity/Plan.java
@@ -0,0 +1,31 @@
+package com.sparta.plannerservice.plan.entity;
+
+import com.sparta.plannerservice.common.entity.UuidEntity;
+import com.sparta.plannerservice.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Plan extends UuidEntity {
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToMany(mappedBy = "plans", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<>();
+
+}
--- src/main/java/com/sparta/plannerservice/user/entity/User.java
+++ src/main/java/com/sparta/plannerservice/user/entity/User.java
@@ -1,9 +1,12 @@
 package com.sparta.plannerservice.user.entity;
 
 import com.sparta.plannerservice.common.entity.UuidEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import com.sparta.plannerservice.plan.entity.Plan;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -19,6 +22,12 @@ public class User extends UuidEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
-
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_plan",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "plan_id")
+    )
+    private Set<Plan> plans = new HashSet<>();
 
 }
