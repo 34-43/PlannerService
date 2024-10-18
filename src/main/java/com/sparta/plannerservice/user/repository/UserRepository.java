e63b694e845dd63612049b89d3695004ec069eed
34-43 <jehyuck3443@gmail.com>
2024-10-18 10:49:31 +0900

    feat: updated User repository, with default method

--- src/main/java/com/sparta/plannerservice/user/repository/UserRepository.java
+++ src/main/java/com/sparta/plannerservice/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.plannerservice.user.repository;
 
+import com.sparta.plannerservice.common.exception.IdNotFoundException;
 import com.sparta.plannerservice.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,8 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+
+    default User findByIdSafe(UUID id) {
+        return this.findById(id).orElseThrow(() -> new IdNotFoundException(User.class, id));
+    }
 }
