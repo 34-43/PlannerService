03579adec3029dd59b56a456ad30c14f6f2d1638
34-43 <jehyuck3443@gmail.com>
2024-10-30 16:29:13 +0900

    chore: minor lint handle

--- src/main/java/com/sparta/plannerservice/plan/dto/WeatherApiFetchDto.java
+++ src/main/java/com/sparta/plannerservice/plan/dto/WeatherApiFetchDto.java
@@ -2,8 +2,10 @@ package com.sparta.plannerservice.plan.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class WeatherApiFetchDto {
     private String date;
